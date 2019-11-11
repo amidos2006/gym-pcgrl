07d8c8e7380f88be0dcb028944e4d9ab158dd557
Philip Bontrager <pbontrager@gmail.com>
2019-11-11 16:01:23 -0500

    Updated running and evalution code

--- rl_test.py
+++ rl_test.py
@@ -8,6 +8,7 @@ from stable_baselines.common.policies import MlpPolicy, CnnPolicy, FeedForwardPo
 from stable_baselines.common.vec_env import SubprocVecEnv, DummyVecEnv
 from stable_baselines.a2c.utils import conv, linear, conv_to_fc
 from stable_baselines.results_plotter import load_results, ts2xy
+from stable_baselines.bench import Monitor
 from stable_baselines import PPO2
 
 import tensorflow as tf
@@ -15,6 +16,7 @@ import numpy as np
 
 import pdb
 
+log_dir = './'
 best_mean_reward, n_steps = -np.inf, 0
 
 def callback(_locals, _globals):
@@ -30,6 +32,7 @@ def callback(_locals, _globals):
         x, y = ts2xy(load_results(log_dir), 'timesteps')
         pdb.set_trace()
         if len(x) > 0:
+            pdb.set_trace()
             mean_reward = np.mean(y[-100:])
             print(x[-1], 'timesteps')
             print("Best mean reward: {:.2f} - Last mean reward per episode: {:.2f}".format(best_mean_reward, mean_reward))
@@ -47,10 +50,11 @@ def callback(_locals, _globals):
 class PCGRL(gym.Wrapper):
     def __init__(self, game):
         self.env = gym.make(game)
+        self.env.adjust_param(random_tile=False, max_iterations = 1000)
         gym.Wrapper.__init__(self, self.env)
 
         #self.observation_space = gym.spaces.Box(low=0, high=1, shape=(198,), dtype=np.float32)
-        self.observation_space = gym.spaces.Box(low=0, high=1, shape=(14, 14, 2), dtype=np.uint8)
+        self.observation_space = gym.spaces.Box(low=0, high=1, shape=(14, 14, 1), dtype=np.uint8)
 
     def step(self, action):
         action = action.item()
@@ -72,9 +76,11 @@ class PCGRL(gym.Wrapper):
         #image
         map = obs['map']
         x, y = obs['pos']
-        pos = np.zeros_like(map)
+        pos = np.zeros([14,14,1])
+        #pos = np.zeros_like(map)
         pos[y][x] = 1
-        return np.stack([map, pos], 2)
+        #return np.stack([map, pos], 2)
+        return pos
 
 def Cnn(image, **kwargs):
     activ = tf.nn.relu
@@ -90,12 +96,13 @@ class CustomPolicy(FeedForwardPolicy):
 
 def main(game, n_cpu):
     # multiprocess environment
+    #env = SubprocVecEnv([(lambda worker: lambda: Monitor(PCGRL(game), log_dir, allow_early_resets=True))(i) for i in range(n_cpu)])
     env = SubprocVecEnv([lambda: PCGRL(game) for i in range(n_cpu)])
     #env = DummyVecEnv([lambda: PCGRL()])
 
-    model = PPO2(CustomPolicy, env, verbose=1, tensorboard_log="./runs")
-    model.learn(total_timesteps=2500000, tb_log_name="Logging_Test")
-    model.save("ppo_binary")
+    model = PPO2(CustomPolicy, env, verbose=0, tensorboard_log="./runs")
+    model.learn(total_timesteps=int(5e6), tb_log_name="Logging_Test") #, callback=callback)
+    #model.save("ppo_binary")
 
     #del model # remove to demonstrate saving and loading
 
