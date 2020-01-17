e2508d7d2af0fe09e91ab1aba47acb4c8d7a042c
Ahmed Abdel Samea Khalifa <amidos2002@hotmail.com>
2020-01-16 22:53:14 -0500

    making only binary

--- gym_pcgrl/envs/probs/__init__.py
+++ gym_pcgrl/envs/probs/__init__.py
@@ -1,5 +1,4 @@
 from gym_pcgrl.envs.probs.binary_prob import BinaryProblem
-from gym_pcgrl.envs.probs.binaryimp_prob import BinaryImpProblem
 from gym_pcgrl.envs.probs.ddave_prob import DDaveProblem
 from gym_pcgrl.envs.probs.mdungeon_prob import MDungeonProblem
 from gym_pcgrl.envs.probs.sokoban_prob import SokobanProblem
@@ -8,7 +7,6 @@ from gym_pcgrl.envs.probs.zelda_prob import ZeldaProblem
 # all the problems should be defined here with its corresponding class
 PROBLEMS = {
     "binary": BinaryProblem,
-    "binaryimp": BinaryImpProblem,
     "ddave": DDaveProblem,
     "mdungeon": MDungeonProblem,
     "sokoban": SokobanProblem,
